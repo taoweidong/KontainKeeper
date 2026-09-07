11f629cdd90adcbab7601f54d6296bed25bf098e
Taowd <taowd@outlook.com>
2026-09-08 05:12:03 +0800

    chore(web): 同步命令中心拆分后的构建产物
    web/dist 经 pnpm build 重出后，server/src/kk_server/web/ 同步刷新（vite 的
    content-hash 文件名因此出现大量新增/删除），重启服务端即可命中新版 SPA。
    本次纯产物同步，源码侧改动见上两条（VITE_PROXY + types/global.d.ts）。

---
+++ server/src/kk_server/web/static/js/CommandHistory-BvbPa-24.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CommandHistory.vue_vue_type_script_setup_true_lang-D8utZ_zD.js";import"./commands-w-ADSjOe.js";import"./index-TjPhoy-N.js";import"./kk-BxMOLmAT.js";export{o as default};
